d903cebf385ce46e4ae871c4707f61522181df96
hanmantguddapure <hanmantguddapure@gmail.com>
2022-07-25 21:41:41 +0530

    removed unwanted code

--- src/main/java/com/api/gateway/service/AuthServiceImpl.java
+++ src/main/java/com/api/gateway/service/AuthServiceImpl.java
@@ -14,33 +14,10 @@ public class AuthServiceImpl implements AuthSerivice {
 
 	@Override
 	public AuthDetailsRequest login(AuthDetailsRequest authDetails) {
-		//UserCredential usercredential = null;
 		AuthDetailsRequest authDetailDto = null;
-		//usercredential = this.getDaoManager().getAuthDao().findByUserName(authDetails.getUsername());
-	/*	if (usercredential != null && usercredential.getPassword().equals(authDetails.getPassword())) {
-			if (Duration.between(usercredential.getTokenTime(), LocalTime.now()).toMinutes() > 5) {
-				authDetailDto = new AuthDetailsRequest();
-				authDetailDto.setStatus("Session time out");
-				return authDetailDto;
-			} e*//*lse {
-				usercredential.setLastLoginTime(LocalDateTime.now());
-				usercredential.setLoggedInUserIpAddress(authDetails.getLoggedInUserIpAddress());
-				usercredential = this.getDaoManager().getAuthDao().saveUserCredential(usercredential);*/
-				// UserRole userRole =
-				// this.getDaoManager().getUserRole().getUserRoleById(usercredential);
-				authDetailDto = new AuthDetailsRequest();
-				authDetailDto.setStatus("Success");
-				/*
-				 * if (userRole != null) authDetailDto.setRoleId(userRole.getRoleId());
-				 */
-			/*	authDetailDto.setUserId(usercredential.getUserId());
-				authDetailDto.setFullName(usercredential.getProfileId().getFullName());
-*/
-				authDetailDto.setJwtToken(jwtUtil.generateToken("hanmant"/*usercredential.getUserName()*/));
-			//}
-	/*
-	 * } else { throw new RecordNotFound("user not found"); }
-	 */
+		authDetailDto = new AuthDetailsRequest();
+		authDetailDto.setStatus("Success");
+		authDetailDto.setJwtToken(jwtUtil.generateToken("hanmant"/* usercredential.getUserName() */));
 		return authDetailDto;
 	}
 
